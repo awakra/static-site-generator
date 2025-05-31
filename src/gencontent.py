eb6eaeacf6060abe9fc25552fd108e8a48838b06
awakra <lucasadls0@gmail.com>
2025-05-30 21:54:45 -0300

    Update gencontent, main and build

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,8 +1,20 @@
 import os
+from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path, basepath)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
+
+
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
@@ -18,6 +30,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown_content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', 'href="' + basepath)
+    template = template.replace('src="/', 'src="' + basepath)
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
@@ -32,28 +46,3 @@ def extract_title(md):
         if line.startswith("# "):
             return line[2:]
     raise ValueError("no title found")
-
-
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
-    """
-    Recursively generates HTML pages for all markdown files in a directory.
-    
-    Args:
-        dir_path_content (str): Path to the content directory containing markdown files
-        template_path (str): Path to the HTML template file
-        dest_dir_path (str): Path to the destination directory for generated HTML files
-    """
-    
-    for entry in os.listdir(dir_path_content):
-        from_path = os.path.join(dir_path_content, entry)
-        
-        if os.path.isfile(from_path):
-            if entry.endswith('.md'):
-                html_filename = entry[:-3] + '.html' 
-                dest_path = os.path.join(dest_dir_path, html_filename)
-                
-                generate_page(from_path, template_path, dest_path)
-        
-        else:
-            subdirectory_dest = os.path.join(dest_dir_path, entry)
-            generate_pages_recursive(from_path, template_path, subdirectory_dest)
